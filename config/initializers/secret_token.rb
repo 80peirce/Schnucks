25cbf9d3451053d94aabb9015ff55e4e6147025d
Adrienne Peirce <adrienne.peirce@gmail.com>
2012-06-29 20:18:46 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schnucks::Application.config.secret_token = '9def1ccb4a845d0874451bf5954ba66ad7016e750faf6b49c44edf08e72ac6cd4c8838382b797dd209e9cbc7a483694df6024a45158c882b35823ff860fe6f18'
