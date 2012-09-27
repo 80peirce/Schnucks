ada760faf71c29bb8bd59f2ab7f9715f7484ca5b
Adrienne Peirce <adrienne.peirce@gmail.com>
2012-09-26 19:51:22 -0500

    email config in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,16 @@ Schnucks::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+    # email configuration
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "mail.adriennepeirce.com",
+  :port                 => 2626,
+  :domain               => 'www.adriennepeirce.com',
+  :user_name            => 'abp@adriennepeirce.com',
+  :password             => 'mecom80!',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
+  
 end
