aa96829b982b8f07b1adec3072afea20506ec8a8
Jacob Vass <jacobvass@gmail.com>
2012-11-20 16:45:10 -0600

    tweak some editing and updating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,10 +37,11 @@ class UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
-    params[:user][:wins] = params[:user][:wins].to_i 
-    params[:user][:total_points] = params[:user][:total_points].to_i
+    #params[:user][:wins] = params[:user][:wins].to_i 
+    #params[:user][:total_points] = params[:user][:total_points].to_i
     if @user.update_attributes(params[:user])
-      redirect_to @user, notice: 'User was successfully updated.'
+      #redirect_to @user, notice: 'User was successfully updated.'
+      redirect_to users_url 
     else
       render 'edit'
     end
