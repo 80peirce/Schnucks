875d7efe8177409aeb2c50d94055a71aac520e7d
Jacob Vass <jacobvass@gmail.com>
2012-11-20 15:56:04 -0600

    add edit user stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,10 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(params[:user])
+    @user.wins = 0
+    @user.total_points = 0
     if @user.save
-      redirect_to @user
+      redirect_to users_url
     else
       render 'new'
     end
@@ -20,6 +22,17 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @users_ranked = @users.sort { |x,y| y.total_points <=> x.total_points }
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    
+    redirect_to users_url
   end
   
 end
