d489287f07d1f5a9a4752a604c6a8d22291c12ce
Jacob Vass <jacobvass@gmail.com>
2012-12-01 14:27:19 -0600

    grr 3

--- db/migrate/20121201021358_add_schucks_team_id_to_hitters.rb
+++ db/migrate/20121201021358_add_schucks_team_id_to_hitters.rb
@@ -1,5 +1,5 @@
 class AddSchucksTeamIdToHitters < ActiveRecord::Migration
   def change
-    add_column :hitters, :schnucks_team_id, :integer
+   # add_column :hitters, :schnucks_team_id, :integer
   end
 end
