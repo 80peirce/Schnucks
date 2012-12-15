48f616cd0f32e8f85013ca17f7de059100794af0
Jacob Vass <jacobvass@gmail.com>
2012-12-14 19:19:16 -0600

    gem messin

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,14 @@ end
 ## trying this to make heroku work, based on
 ## https://devcenter.heroku.com/articles/rails3x-asset-pipeline-cedar#troubleshooting
 
-config.assets.initialize_on_precompile = false
+
 
 module Schnucks
   class Application < Rails::Application
 
- 
+  config.assets.initialize_on_precompile = false
+  
+   
  # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
