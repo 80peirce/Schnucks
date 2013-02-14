3cb1d1874dee14f26e7470b267ad93c33eba3237
Jacob Vass <jacobvass@gmail.com>
2013-02-13 21:57:10 -0600

    everything to_i

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -91,7 +91,7 @@ class PlayController < ApplicationController
     def schnucks_score_lower(total1, total2)
       if (total1 * 10000).to_i <  (total2 * 10000).to_i
         [2,1]
-      elsif (total1 * 10000).to_i > (total2 * 10000).to_i  
+      elsif (total1 * 10000).to_i > (total2 * 10000).to_i
         [1,2]
       else
         [1.5,1.5]
@@ -101,13 +101,6 @@ class PlayController < ApplicationController
     
     @team1 = SchnucksGame.find(session[:todays_game_id]).schnucks_teams[0]
     @team2 = SchnucksGame.find(session[:todays_game_id]).schnucks_teams[1]
-    
-    
-    #@hitters_team1 = Hitter.find_all_by_schnucks_team_id(0)
-    #@pitchers_team1 = Pitcher.find_all_by_schnucks_team_id(0) 
- 
-    #@hitters_team2 = Hitter.find_all_by_schnucks_team_id(1)
-    #@pitchers_team2 = Pitcher.find_all_by_schnucks_team_id(1)
         
     
     @team1.hitters = Hitter.find_all_by_schnucks_team_id(0)
@@ -152,20 +145,19 @@ class PlayController < ApplicationController
     
 
     @team1.pitchers.each do |p|
-      #@team1_ip += p.innings_pitched
-     # @team1_ip += 1 
-     # @team1_er += p.earned_runs
-     # @team1_hits_allowed += p.hits
-     # @team1_bb += p.walks
+      @team1_ip += p.innings_pitched.to_f
+      @team1_er += p.earned_runs.to_i
+      @team1_hits_allowed += p.hits.to_i
+      @team1_bb += p.walks.to_i
       @team1_k += p.strikeouts.to_i
-     # @team1_wins += p.wins
-     # @team1_losses += p.losses
-     # @team1_saves += p.saves
+      @team1_wins += p.wins.to_i
+      @team1_losses += p.losses.to_i
+      @team1_saves += p.saves.to_i
     end
     
     
-    #@team1_era = (@team1_er.to_f / @team1_ip.to_f) * 9.to_f
-    #@team1_whip = (@team1_bb.to_f + @team1_hits_allowed.to_f)/ @team1_ip.to_f
+    @team1_era = (@team1_er.to_f / @team1_ip) * 9.to_f
+    @team1_whip = (@team1_bb.to_f + @team1_hits_allowed.to_f)/ @team1_ip.to_f
 
     
     @team2_ab = 0
@@ -196,23 +188,20 @@ class PlayController < ApplicationController
     @team2_losses = 0
     @team2_saves = 0
     
-=begin
     @team2.pitchers.each do |p|
-      #@team1_ip += p.innings_pitched
-      @team2_ip += 1
-      @team2_er += p.earned_runs
-      @team2_hits_allowed += p.hits
-      @team2_bb += p.walks
-      @team2_k += p.strikeouts
-      @team2_wins += p.wins
-      @team2_losses += p.losses
-      @team2_saves += p.saves
+      @team2_ip += p.innings_pitched.to_f
+      @team2_er += p.earned_runs.to_i
+      @team2_hits_allowed += p.hits.to_i
+      @team2_bb += p.walks.to_i
+      @team2_k += p.strikeouts.to_i
+      @team2_wins += p.wins.to_i
+      @team2_losses += p.losses.to_i
+      @team2_saves += p.saves.to_i
     end
     
-    @team2_era = (@team2_er.to_f / @team2_ip.to_f) * 9.to_f
+    @team2_era = (@team2_er.to_f / @team2_ip) * 9.to_f
     @team2_whip = (@team2_bb.to_f + @team2_hits_allowed.to_f)/ @team2_ip.to_f
 
-=end
   
   @team1.game_score += schnucks_score_higher(@team1_hits,@team2_hits)[0]
   @team2.game_score += schnucks_score_higher(@team1_hits,@team2_hits)[1]
@@ -231,9 +220,9 @@ class PlayController < ApplicationController
   @team1.game_score += schnucks_score_higher(@team1_saves,@team2_saves)[0]
   @team2.game_score += schnucks_score_higher(@team1_saves,@team2_saves)[1]
 
-  #@team1.game_score += schnucks_score_lower(@team1_era,@team2_era)[0]
-  #@team2.game_score += schnucks_score_lower(@team1_era,@team2_era)[1]
-  #@team1.game_score += schnucks_score_lower(@team1_whip,@team2_whip)[0]
-  #@team2.game_score += schnucks_score_lower(@team1_whip,@team2_whip)[1]
+  @team1.game_score += schnucks_score_lower(@team1_era,@team2_era)[0]
+  @team2.game_score += schnucks_score_lower(@team1_era,@team2_era)[1]
+  @team1.game_score += schnucks_score_lower(@team1_whip,@team2_whip)[0]
+  @team2.game_score += schnucks_score_lower(@team1_whip,@team2_whip)[1]
   end
 end
