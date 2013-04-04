4f77e0cd9d364cf7aeef02c738485592df48b14a
Adrienne Peirce <adrienne.peirce@gmail.com>
2013-04-03 19:56:35 -0500

    remove user validations for dev

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,17 +20,17 @@
 class User < ActiveRecord::Base
   attr_accessible :losses, :name, :total_points, :wins, :email, :rps_wins, :rocks, :papers, :scissorses, :password, :password_confirmation
   
-  has_secure_password
+#  has_secure_password
 
-  before_save { |user| user.email = email.downcase }
+#  before_save { |user| user.email = email.downcase }
 
-  validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence:   true,
-                    format:     { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true
+#  validates :name, presence: true, length: { maximum: 50 }
+#  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+#  validates :email, presence:   true,
+#                    format:     { with: VALID_EMAIL_REGEX },
+#                    uniqueness: { case_sensitive: false }
+#  validates :password, presence: true, length: { minimum: 6 }
+#  validates :password_confirmation, presence: true
 
   #commented out validations for short term editing administration of schucks data for tracking
   
